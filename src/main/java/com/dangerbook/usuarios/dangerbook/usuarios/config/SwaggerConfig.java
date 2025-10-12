8fe7f167433d62e30cd397c9bdc1bee895f39e38
Nicolás <nico.vallejos@duocuc.cl>
2025-10-12 18:57:42 -0300

    Swagger implementado
    Se implemento el Swagger.

---
+++ src/main/java/com/dangerbook/usuarios/dangerbook/usuarios/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.dangerbook.usuarios.dangerbook.usuarios.config;
+
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Usuarios de DangerBook")
+                        .version("1.0")
+                        .description("Documentacion de la API de DangerBook para la sección de Usuarios"));
+    }
+}
